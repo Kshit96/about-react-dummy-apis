534abf10b14aa684f6449ae8d80e0a20c0875144
Kshit96 <kshit96@gmail.com>
2021-07-14 20:42:30 +0530

    Update package.json with socket.io and https dependencies

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 
 var app = express();
-const server = require("http").createServer(app);
+const server = require("https").createServer(app);
 const io = require("socket.io").listen(server);
 const port = 3000;
 
