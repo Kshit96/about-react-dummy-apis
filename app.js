2318dd42f2b809607ae8553dc770ca524845f038
Kshit96 <kshit96@gmail.com>
2021-07-14 18:40:12 +0530

    Add socket.io for chat functoinality

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 
 var app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const port = 3000;
+
+io.on("connection", socket => {
+  console.log('A user connected :D');
+  socket.on("chat message", msg => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  })
+})
+
+server.listen(port, () => console.log("server running on port:" + port));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
